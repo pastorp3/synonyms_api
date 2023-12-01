bd3c57b660ffad3618140e6c740779dd3f3a3a50
JOSE PEDRAZA <pastorpedraza3@gmail.com>
2023-12-01 13:06:42 -0600

    Add mutation to review word and synonyms

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,7 @@ module Types
   class MutationType < Types::BaseObject
     field :admin_login, mutation: Mutations::AdminLogin
     field :add_synonym, mutation: Mutations::AddSynonym
+    field :review_word, mutation: Mutations::ReviewWord
+    field :review_synonym, mutation: Mutations::ReviewSynonym
   end
 end
